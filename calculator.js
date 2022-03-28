b8a90d03d135fc35d62b201e6a6629c3acd2ef29
LianNidam <liannidam04@gmail.com>
2022-03-28 14:07:16 +0300

    check

--- calculator.js
+++ calculator.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req,res){
+  res.send("Thanks for posting that!");
+});
+
 app.listen(3000, function(){
   console.log("server is running on port 3000");
 });
