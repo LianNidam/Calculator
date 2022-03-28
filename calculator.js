3e704e0482e984209301c72bf400f125388b2071
LianNidam <liannidam04@gmail.com>
2022-03-28 22:43:45 +0300

    done

--- calculator.js
+++ calculator.js
@@ -1,14 +1,23 @@
 //jshint esversion:6
 
 const express= require("express");
+const bodyParser= require("body-parser");
 
 const app= express();
+app.use(bodyParser.urlencoded({extanded:true}));
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req,res){
-  res.send("Thanks for posting that!");
+
+  var num1=Number(req.body.num1);
+  var num2=Number(req.body.num2);
+
+  var result= num1 + num2;
+
+  res.send("The result of the calculator is " + result);
 });
 
 app.listen(3000, function(){
