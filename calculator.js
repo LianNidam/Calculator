2ee906e94b5b0f6b5985aa6bb1c353acd6d1b2b2
LianNidam <liannidam04@gmail.com>
2022-03-28 23:14:54 +0300

    bmi calculate also done

--- calculator.js
+++ calculator.js
@@ -20,6 +20,19 @@ app.post("/", function(req,res){
   res.send("The result of the calculator is " + result);
 });
 
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+  var weight= parseFloat(req.body.weight);
+  var height= parseFloat(req.body.height);
+
+  var bmi= weight / (height*height);
+
+  res.send("Your BMI is " + bmi);
+});
+
 app.listen(3000, function(){
   console.log("server is running on port 3000");
 });
